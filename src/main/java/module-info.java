5208f01d6dbc004ac578682eef70a1e136dbc02e
baardl <bard.lind@gmail.com>
2018-03-08 08:58:17 +0100

    Weave modules together. Test by creating owner.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,6 @@ module no.cantara.java.modules.json2 {
     requires slf4j.api;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
+
+    exports no.cantara.java.modules.json2;
 }
